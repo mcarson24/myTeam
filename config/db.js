2eeee3bd4ee5c67e496774932b11f31c3365f8b3
Matthew Carson <matthew.carson@pm.me>
2022-04-17 00:34:19 -0400

    Stop logging the executed SQL to the terminal

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ import { Sequelize } from 'sequelize'
 
 const sequelize = new Sequelize('myTeam', 'root', '', {
   host: 'localhost',
-  dialect: "mysql"
+  dialect: "mysql",
+  logging: false
 })
 
 export { sequelize }
